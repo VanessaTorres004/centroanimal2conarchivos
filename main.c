a895ac4feba336340c1c42007f8cbc9f765c29d8
VanessaTorres004 <130101295+VanessaTorres004@users.noreply.github.com>
2023-07-14 13:47:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 #define MAX_TIPO 30
 #define MAX_EDAD 30
 #define MAX_FAMILIAR 30
+#define MAX_LINE_SIZE 100
 
 void ingresarDatosFacturacion(const char *ID, const char *NOMBRE, const char *TIPO, const char *EDAD, const char *FAMILIAR, float *cantidad, float *precio, float *subtotal, int cantidadProductos, float total) {
-	FILE *file = fopen("factura.txt", "w"); // Open the file in write mode
+	FILE *file = fopen("factura.txt", "a"); // Abrir el archivo en modo append
 	if (file == NULL) {
 		printf("No se pudo abrir el archivo.\n");
 		return;
@@ -16,7 +17,7 @@ void ingresarDatosFacturacion(const char *ID, const char *NOMBRE, const char *TI
 	fprintf(file, "############Datos de facturacion ##########\n");
 	fprintf(file, "ID de la mascota: %s\n", ID);
 	
-	fprintf(file, " ** FACTURA **\n");
+	fprintf(file, " * FACTURA *\n");
 	fprintf(file, "PRODUCTO\t\tCANTIDAD\tVALOR\t\tUNITARIO\n");
 	for (int i = 0; i < cantidadProductos; i++) {
 		fprintf(file, "%d\t\t\t%.1f\t\t%.2f\t\t%.2f\n", i+1, cantidad[i], precio[i], subtotal[i]);
@@ -25,14 +26,36 @@ void ingresarDatosFacturacion(const char *ID, const char *NOMBRE, const char *TI
 	fprintf(file, "El total es %.2f\n", total);
 	fprintf(file, "Gracias por venir\n");
 	
-	fclose(file); // Close the file
+	fclose(file);
+}
+
+void guardarFacturaEnArchivo(const char *ID, float *cantidad, float *precio, float *subtotal, int cantidadProductos, float total) {
+	FILE *file = fopen("factura.txt", "a");
+	if (file == NULL) {
+		printf("No se pudo abrir el archivo.\n");
+		return;
+	}
+	
+	fprintf(file, "############Datos de facturacion ##########\n");
+	fprintf(file, "ID de la mascota: %s\n", ID);
+	
+	fprintf(file, " * FACTURA *\n");
+	fprintf(file, "PRODUCTO\t\tCANTIDAD\tVALOR\t\tUNITARIO\n");
+	for (int i = 0; i < cantidadProductos; i++) {
+		fprintf(file, "%d\t\t\t%.1f\t\t%.2f\t\t%.2f\n", i+1, cantidad[i], precio[i], subtotal[i]);
+	}
+	
+	fprintf(file, "El total es %.2f\n", total);
+	fprintf(file, "Gracias por venir\n");
+	
+	fclose(file);
 }
 
 void imprimirFactura(const char *ID, float *cantidad, float *precio, float *subtotal, int cantidadProductos, float total) {
 	printf("############Datos de facturacion ##########\n");
 	printf("ID de la mascota: %s\n", ID);
 	
-	printf(" ** FACTURA **\n");
+	printf(" * FACTURA *\n");
 	printf("PRODUCTO\t\tCANTIDAD\tVALOR\t\tUNITARIO\n");
 	for (int i = 0; i < cantidadProductos; i++) {
 		printf("%d\t\t\t%.1f\t\t%.2f\t\t%.2f\n", i+1, cantidad[i], precio[i], subtotal[i]);
@@ -40,6 +63,8 @@ void imprimirFactura(const char *ID, float *cantidad, float *precio, float *subt
 	
 	printf("El total es %.2f\n", total);
 	printf("Gracias por venir\n");
+	
+	guardarFacturaEnArchivo(ID, cantidad, precio, subtotal, cantidadProductos, total); // Guardar factura en el archivo
 }
 
 int main() {
@@ -65,27 +90,62 @@ int main() {
 		scanf(" %c", &SN);
 		
 		switch (SN) {
-		case 'a':
+		case 'a': {
+			FILE *f1;
+			f1 = fopen("mascota.txt", "w");
+			
+			if (f1 == NULL) {
+				printf("No se pudo abrir el archivo.\n");
+				return 1;
+			}
+			
 			printf("ID de la mascota: ");
 			scanf("%s", ID);
 			printf("Nombre de la mascota: ");
 			scanf("%s", NOMBRE);
-			printf("tipo:");
+			printf("Tipo: ");
 			scanf("%s", TIPO);
-			printf("edad:");
+			printf("Edad: ");
 			scanf("%s", EDAD);
-			printf("nombre del dueño:");
+			printf("Nombre del dueño: ");
 			scanf("%s", FAMILIAR);
-			printf("registro completo\n");
+			
+			fprintf(f1, "ID de la mascota: %s\n", ID);
+			fprintf(f1, "Nombre de la mascota: %s\n", NOMBRE);
+			fprintf(f1, "Tipo: %s\n", TIPO);
+			fprintf(f1, "Edad: %s\n", EDAD);
+			fprintf(f1, "Nombre del dueño: %s\n", FAMILIAR);
+			
+			fclose(f1);
+			
+			printf("Registro completo.\n");
+			
 			break;
-		case 'b':
-			printf("***ELIJA un servicio***\n");
-			printf("ID:1 nombre:peluqueria descripcion: baño,corte de pelo y uñas precio($15)\n");
-			printf("ID:2 nombre:veterinario descripcion:chequeo general precio($20)\n");
-			printf("ID:3 nombre:vacunacion descripcion:vacunas precio($10)\n");
-			printf("ID:4 nombre:desparacitacion descripcion: se desparacita al animal precio($18)\n");
-			printf("ID:5 nombre:hotel  descripcion:se cuida al animal x noche precio($20)\n");
+		}
+		
+		case 'b': {
+			FILE *f2;
+			f2 = fopen("servicios.txt", "r");
+			if (f2 == NULL) {
+				printf("No se pudo abrir el archivo de servicios.\n");
+				return 1;
+			}
+			
+			char linea[MAX_LINE_SIZE];
+			while (fgets(linea, MAX_LINE_SIZE, f2) != NULL) {
+				printf("%s\n", linea);
+			}
+			
+			fclose(f2);
+			
+			printf("Ingrese la opción: \n");
 			scanf(" %c", &opcion);
+			
+			while (opcion < '1' || opcion > '5') {
+				printf("Opcion invalida\n");
+				printf("Ingrese la opción: ");
+				scanf(" %c", &opcion);
+			}
 			
 			float *producto_cantidad = &cantidad[cantidadProductos];
 			float *producto_precio = &precio[cantidadProductos];
@@ -118,7 +178,6 @@ int main() {
 				*producto_precio = 20;
 				break;
 			default:
-				printf("Opcion invalida\n");
 				break;
 			}
 			
@@ -127,17 +186,20 @@ int main() {
 			cantidadProductos++;
 			
 			break;
+		}
+		
 		case 'c':
 			ingresarDatosFacturacion(ID, NOMBRE, TIPO, EDAD, FAMILIAR, cantidad, precio, subtotal, cantidadProductos, total);
-			imprimirFactura(ID, cantidad, precio, subtotal, cantidadProductos, total);
-			return 0;
+			break;
+			
 		default:
 			break;
 		}
 	} while (SN != 'd');
 	
+	imprimirFactura(ID, cantidad, precio, subtotal, cantidadProductos, total); // Imprimir factura al salir
+	
 	printf("Gracias por venir\n");
 	
 	return 0;
 }
-
