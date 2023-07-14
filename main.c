ed6dbcf75bf0c43f6ac506f83ffff2d86e169162
VanessaTorres004 <130101295+VanessaTorres004@users.noreply.github.com>
2023-07-14 15:33:15 -0500

    Update main.c

--- main.c
+++ main.c
@@ -27,6 +27,19 @@ void ingresarDatosFacturacion(const char *ID, const char *NOMBRE, const char *TI
 	fprintf(file, "Gracias por venir\n");
 	
 	fclose(file);
+	
+	// Imprimir factura en la consola
+	printf("############Datos de facturacion ##########\n");
+	printf("ID de la mascota: %s\n", ID);
+	
+	printf(" * FACTURA *\n");
+	printf("PRODUCTO\t\tCANTIDAD\tVALOR\t\tUNITARIO\n");
+	for (int i = 0; i < cantidadProductos; i++) {
+		printf("%d\t\t\t%.1f\t\t%.2f\t\t%.2f\n", i+1, cantidad[i], precio[i], subtotal[i]);
+	}
+	
+	printf("El total es %.2f\n", total);
+	printf("Gracias por venir\n");
 }
 
 void guardarFacturaEnArchivo(const char *ID, float *cantidad, float *precio, float *subtotal, int cantidadProductos, float total) {
@@ -67,6 +80,43 @@ void imprimirFactura(const char *ID, float *cantidad, float *precio, float *subt
 	guardarFacturaEnArchivo(ID, cantidad, precio, subtotal, cantidadProductos, total); // Guardar factura en el archivo
 }
 
+void modificarDatosMascota(char *ID, char *NOMBRE, char *TIPO, char *EDAD, char *FAMILIAR) {
+	char opcion;
+	printf("Qué campo desea modificar?\n");
+	printf("a) ID\n");
+	printf("b) Nombre\n");
+	printf("c) Tipo\n");
+	printf("d) Edad\n");
+	printf("e) Familiar\n");
+	scanf(" %c", &opcion);
+	
+	switch (opcion) {
+	case 'a':
+		printf("Nuevo ID: ");
+		scanf("%s", ID);
+		break;
+	case 'b':
+		printf("Nuevo Nombre: ");
+		scanf("%s", NOMBRE);
+		break;
+	case 'c':
+		printf("Nuevo Tipo: ");
+		scanf("%s", TIPO);
+		break;
+	case 'd':
+		printf("Nueva Edad: ");
+		scanf("%s", EDAD);
+		break;
+	case 'e':
+		printf("Nuevo Familiar: ");
+		scanf("%s", FAMILIAR);
+		break;
+	default:
+		printf("Opción inválida.\n");
+		break;
+	}
+}
+
 int main() {
 	char ID[MAX_CEDULA];
 	char NOMBRE[MAX_NOMBRE];
@@ -82,17 +132,18 @@ int main() {
 	char SN;
 	
 	do {
-		printf("Que desea el dia de hoy?\n");
+		printf("Qué desea el día de hoy?\n");
 		printf("a) Ingresar mascota\n");
 		printf("b) Servicio\n");
-		printf("c) Imprimir factura\n");
-		printf("d) Salir\n");
+		printf("c) Modificar datos de la mascota\n");
+		printf("d) Imprimir factura\n");
+		printf("e) Salir\n");
 		scanf(" %c", &SN);
 		
 		switch (SN) {
 		case 'a': {
 			FILE *f1;
-			f1 = fopen("mascota.txt", "w");
+			f1 = fopen("mascota.txt", "a");
 			
 			if (f1 == NULL) {
 				printf("No se pudo abrir el archivo.\n");
@@ -142,7 +193,7 @@ int main() {
 			scanf(" %c", &opcion);
 			
 			while (opcion < '1' || opcion > '5') {
-				printf("Opcion invalida\n");
+				printf("Opción inválida\n");
 				printf("Ingrese la opción: ");
 				scanf(" %c", &opcion);
 			}
@@ -189,13 +240,17 @@ int main() {
 		}
 		
 		case 'c':
+			modificarDatosMascota(ID, NOMBRE, TIPO, EDAD, FAMILIAR);
+			break;
+			
+		case 'd':
 			ingresarDatosFacturacion(ID, NOMBRE, TIPO, EDAD, FAMILIAR, cantidad, precio, subtotal, cantidadProductos, total);
 			break;
 			
 		default:
 			break;
 		}
-	} while (SN != 'd');
+	} while (SN != 'e');
 	
 	imprimirFactura(ID, cantidad, precio, subtotal, cantidadProductos, total); // Imprimir factura al salir
 	
@@ -203,3 +258,4 @@ int main() {
 	
 	return 0;
 }
+
